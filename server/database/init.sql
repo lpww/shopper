bd74ae4b77c2f65ba6156c73676d933c8d1fd18a
thomas <thomas.treffry@nearform.com>
2022-04-17 10:01:58 -0700

    feat/database: initialize postgres with the schema and test data

---
+++ server/database/init.sql
@@ -0,0 +1,13 @@
+-- create table schema
+CREATE TABLE items(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  description TEXT,
+  quantity INT,
+  completed BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- add test data
+INSERT INTO items (name, description, quantity, completed)
+  VALUES ('bacon', 'smoked', 5, true),
+  ('eggs', NULL, NULL, false);
