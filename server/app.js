cb509e7626a8ff128e2871e44f9f3bd841961457
thomas <thomas.treffry@nearform.com>
2022-04-17 12:49:28 -0700

    feat/server: add /graphql route with mercurius

--- server/app.js
+++ server/app.js
@@ -1,25 +1,29 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require("path");
+const autoLoad = require("fastify-autoload");
+const mercurius = require("mercurius");
+
+const graphql = require("./graphql");
 
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
+  fastify.register(mercurius, {
+    graphiql: "playground",
+    ...graphql,
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+  fastify.register(autoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+  fastify.register(autoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
