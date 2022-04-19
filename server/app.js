e457076df0995b35968ea0aabf61ff3b97999030
thomas <thomas.treffry@nearform.com>
2022-04-19 16:13:22 -0700

    feat/server: add fastify-postgres plugin

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 "use strict";
 
-const path = require("path");
 const autoLoad = require("fastify-autoload");
 const mercurius = require("mercurius");
+const path = require("path");
+const postgres = require("fastify-postgres");
 
 const graphql = require("./graphql");
 
 module.exports = async function (fastify, opts) {
+  // register fastify-postgres, the postgres plugin
+  fastify.register(postgres, {
+    connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DB}`,
+  });
+
+  // register mercurius, the graphql plugin
   fastify.register(mercurius, {
     graphiql: true,
     ...graphql,
