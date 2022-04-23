2be16486ffb20ab8d84086030e2dadbec414a281
thomas <thomas.treffry@nearform.com>
2022-04-23 14:38:04 -0700

    feat/client: add NunitoTypography component

---
+++ client/src/components/NunitoTypography.js
@@ -0,0 +1,8 @@
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+const NunitoTypography = styled(Typography)`
+  font-family: Nunito, san-serif;
+`;
+
+export default NunitoTypography;
