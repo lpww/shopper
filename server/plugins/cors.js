0372d51ee2a0f2dfbd1c1ef2c622716c02d01b94
thomas <thomas.treffry@nearform.com>
2022-04-23 16:40:01 -0700

    feat/server: add fastify-cors plugin

---
+++ server/plugins/cors.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+const cors = require("fastify-cors");
+
+/**
+ * This plugins adds cors configuration
+ *
+ * @see https://github.com/fastify/fastify-cors
+ */
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(cors);
+});
