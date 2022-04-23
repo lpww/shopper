3d6ee977cf3f433d12e269f6f2a912067596eca5
thomas <thomas.treffry@nearform.com>
2022-04-23 14:59:49 -0700

    style/client: add theme

---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+import createTheme from "@mui/material/styles/createTheme";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+      light: "#42a5f5",
+      dark: "#4D81B7",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
+
+export default theme;
