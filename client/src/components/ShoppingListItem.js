eb569dafdb52f3c3ff4fcf14365e030aa70c033a
thomas <thomas.treffry@nearform.com>
2022-04-24 10:57:59 -0700

    feat/client: add ShoppingListItem component

---
+++ client/src/components/ShoppingListItem.js
@@ -0,0 +1,67 @@
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import EditIcon from "@mui/icons-material/EditOutlined";
+
+import styled from "@mui/material/styles/styled";
+
+import FlexColumn from "components/FlexColumn";
+import FlexRow from "components/FlexRow";
+import NunitoTypography from "components/NunitoTypography";
+
+const Typography = styled(NunitoTypography)`
+  font-weight: 500;
+  text-decoration: ${(props) =>
+    props.completed === "true" ? "line-through" : "none"};'
+`;
+
+const Layout = styled(FlexRow)`
+  height: ${(props) => props.theme.spacing(10)};
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid ${(props) => props.theme.palette.grey["200"]};
+  margin: ${(props) => props.theme.spacing(1, 0)};
+  background-color: ${(props) =>
+    props.completed === "true"
+      ? props.theme.palette.primary.light
+      : props.theme.palette.common.white};'
+`;
+
+const ThemedCheckbox = styled(Checkbox)`
+  color: ${(props) => props.theme.palette.text.secondary};
+`;
+
+const ShoppingListItem = (props) => {
+  const { completed, description, name } = props;
+
+  return (
+    <Layout completed={completed.toString()}>
+      <FlexRow>
+        <ThemedCheckbox checked={completed} onChange={() => alert("update")} />
+        <FlexColumn>
+          <Typography completed={completed.toString()} variant="subtitle1">
+            {name}
+          </Typography>
+          <Typography
+            completed={completed.toString()}
+            color="text.secondary"
+            variant="body2"
+          >
+            {description}
+          </Typography>
+        </FlexColumn>
+      </FlexRow>
+      <div>
+        <IconButton onClick={() => alert("edit")} sx={{ marginRight: 1 }}>
+          <EditIcon color="action" />
+        </IconButton>
+        <IconButton onClick={() => alert("delete")} sx={{ marginRight: 2 }}>
+          <DeleteIcon color="action" />
+        </IconButton>
+      </div>
+    </Layout>
+  );
+};
+
+export default ShoppingListItem;
