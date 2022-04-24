2fea2a4ea6c13fd3f9eb77344174f36ec0715aca
thomas <thomas.treffry@nearform.com>
2022-04-24 10:57:37 -0700

    feat/client: add FlexColumn component

---
+++ client/src/components/FlexColumn.js
@@ -0,0 +1,9 @@
+import styled from "@mui/material/styles/styled";
+
+import FlexRow from "components/FlexRow";
+
+const FlexColumn = styled(FlexRow)`
+  flex-direction: column;
+`;
+
+export default FlexColumn;
