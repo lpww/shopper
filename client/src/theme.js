3940b81f3e9ac279651cd7c27159c418b8ae798c
thomas <thomas.treffry@nearform.com>
2022-04-24 10:56:57 -0700

    style/client: add custom theme colors

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,9 +3,19 @@ import createTheme from "@mui/material/styles/createTheme";
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#1976d2",
+      light: "#F8FAFB",
       main: "#4D81B7",
       contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#1871E8",
+    },
+    action: {
+      main: "#555F7C",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#7D7A7A",
     },
   },
 });
