e15f56340f8e3bb3b0963a302f4bf222afe26bf1
thomas <thomas.treffry@nearform.com>
2022-04-24 16:37:46 -0700

    chore/database: remove test data

--- server/database/init.sql
+++ server/database/init.sql
@@ -7,8 +7,3 @@ CREATE TABLE items(
   completed BOOLEAN NOT NULL DEFAULT FALSE,
   deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
-
--- add test data
-INSERT INTO items (name, description, quantity, completed, deleted)
-  VALUES ('bacon', 'smoked', 3, true, false),
-  ('eggs', NULL, NULL, false, false);
